c3c7e2bfc0695459bfcf0e9e53d0e6d960e7e185
a <a>
2024-06-06 12:49:08 +0200

    Interfaces models

---
+++ interfaces.ts
@@ -0,0 +1,23 @@
+export interface Clients {
+	id: number;
+	name: string;
+	address: string;
+	phone: string;
+}
+
+export interface Products {
+	id: number;
+	name: string;
+	price: number;
+	stock: number;
+}
+
+export interface Workers {
+	id: number;
+	name: string;
+}
+
+export interface Sells {
+	id: number;
+	name: string;
+}
