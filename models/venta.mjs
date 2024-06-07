787cccabf85169ee48f83fa96e0d3df9ad3af60a
a <a>
2024-06-07 19:05:26 +0200

    Functional model

--- models/venta.mjs
+++ models/venta.mjs
@@ -1,22 +1,41 @@
 import sequelize from "../db-connection.mjs";
 import { DataTypes } from 'sequelize';
+import Producto from "./producto.mjs";
+import Cliente from "./cliente.mjs";
+import Empleado from "./empleado.mjs";
 
 const Venta = sequelize.define('Venta', {
-	producto: {
-		type: DataTypes.STRING,
-		allowNull: false
-	},
-	cliente: {
-		type: DataTypes.STRING,
-		allowNull: false
-	},
-	empleado: {
+	productoId: {
 		type: DataTypes.INTEGER,
-		allowNull: false
-	}
+		allowNull: false,
+		references: {
+			model: Producto,
+			key: 'id'
+		},
+	},
+	clienteId: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		references: {
+			model: Cliente,
+			key: 'id'
+		},
+	},
+	empleadoId: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		references: {
+			model: Empleado,
+			key: 'id'
+		},
+	},
 }, {
 	updatedAt: false,
 	createdAt: 'fecha_venta',
 });
+
+Venta.belongsTo(Producto, { foreignKey: 'productoId' });
+Venta.belongsTo(Cliente, { foreignKey: 'clienteId' });
+Venta.belongsTo(Empleado, { foreignKey: 'empleadoId' });
 
 export default Venta;
