4119ef9ed42a12f661dc6ac4c695380223b215e6
a <a>
2024-06-08 09:43:31 +0200

    Added password

--- models/cliente.mjs
+++ models/cliente.mjs
@@ -1,10 +1,21 @@
 import connection from "../db-connection.mjs";
 import { DataTypes } from 'sequelize';
+import { createHash } from 'node:crypto';
 
 const Cliente = connection.define('Cliente', {
 	nombre: {
 		type: DataTypes.STRING,
 		allowNull: false
+	},
+	password: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		set(value) {
+			const HASH = createHash('sha256');
+			HASH.update(value);
+			const HASHED_PASSWORD = HASH.digest('hex');
+			this.setDataValue('password', HASHED_PASSWORD);
+		},
 	},
 	direccion: {
 		type: DataTypes.STRING,
