88603c3f04a9dde80ec5cc90f3b20eb7eaa24e79
a <a>
2024-06-08 08:59:55 +0200

    Added patch

---
+++ routes/patch/empleado.mjs
@@ -0,0 +1,26 @@
+const patchEmpleado = (app, model) => {
+	const RUTA = '/empleados/:id';
+	app.patch(RUTA, async (req, res) => {
+		try {
+			const ID = req.params.id;
+			const NOMBRE = req.body?.nombre;
+			const PASSWORD = req.body?.password;
+
+			const EMPLEADO = await model.findOne({ where: { id: ID } });
+			EMPLEADO.set({
+				nombre: NOMBRE,
+				password: PASSWORD
+			});
+
+			await EMPLEADO.save();
+
+			return res.json({ EmpleadoActualizado: EMPLEADO });
+		}
+		catch (error) {
+			console.log('Error', error);
+			return res.status(500).json({ message: 'Internal server error' });
+		}
+	});
+}
+
+export default patchEmpleado;
